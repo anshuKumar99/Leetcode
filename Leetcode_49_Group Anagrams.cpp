32be2bd323e8d544b08ec9c498c80882791f6e4f
anshuKumar99 <148548385+anshuKumar99@users.noreply.github.com>
2024-02-07 00:10:50 +0530

    Update Leetcode_49_Group Anagrams.cpp

--- Leetcode_49_Group Anagrams.cpp
+++ Leetcode_49_Group Anagrams.cpp
@@ -27,7 +27,9 @@ Constraints:
 strs[i] consists of lowercase English letters.
 */
 
-//Approach 1:
+//Approach 1 : Using sorting
+//T.C : O(n*mlog(m)) -> n=input size, m=maximum string length in input vector
+//S.C : O(n*m)
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
@@ -38,6 +40,41 @@ public:
             string curr=strs[i];
             sort(curr.begin(),curr.end());
             mp[curr].push_back(strs[i]);
+        }
+        for(auto i:mp){
+            output.push_back(i.second);
+        }
+        return output;
+
+    }
+};
+
+//Approach 2:
+//T.C : O(n*m) -> n=input size, m=maximum string length in input vector
+//S.C : O(n*m)
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n=strs.size();
+        vector<vector<string>> output;
+        unordered_map<string,vector<string>> mp;
+        for(int i=0;i<n;i++){
+            string curr=strs[i];
+            int arr[26]={0};
+            for(int j=0;j<curr.size();j++){
+                arr[curr[j]-'a']++;
+            }
+            string currSort="";
+            for(int k=0;k<26;){
+                if(arr[k]>0){
+                    currSort+=k+'a';
+                    arr[k]--;
+                }
+                else{
+                    k++;
+                }
+            }
+            mp[currSort].push_back(strs[i]);
         }
         for(auto i:mp){
             output.push_back(i.second);
