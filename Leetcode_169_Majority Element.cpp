99cc8fa80dbc7f531869ddc6b35e5f6b6281fea7
anshuKumar99 <148548385+anshuKumar99@users.noreply.github.com>
2024-02-12 18:14:52 +0530

    Create Leetcode_169_Majority Element.cpp

---
+++ Leetcode_169_Majority Element.cpp
@@ -0,0 +1,41 @@
+/*
+LEETCODE LINK : https://leetcode.com/problems/majority-element/description/
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int> mp;
+        for(auto i:nums){
+            mp[i]++;
+        }
+        for(auto j:mp){
+            if(j.second>(n/2)){
+                return j.first;
+            }
+        }
+        return -1;
+        
+    }
+};
