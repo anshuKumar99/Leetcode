8a489d038a851a8c8bc8929f1fab80e0dee411a4
anshuKumar99 <148548385+anshuKumar99@users.noreply.github.com>
2024-02-16 17:22:50 +0530

    Create Leetcode_2971_Find Polygon With the Largest Perimeter.cpp

---
+++ Leetcode_2971_Find Polygon With the Largest Perimeter.cpp
@@ -0,0 +1,81 @@
+/*
+LEETCODE LINK : https://leetcode.com/problems/find-polygon-with-the-largest-perimeter/description/
+
+You are given an array of positive integers nums of length n.
+
+A polygon is a closed plane figure that has at least 3 sides. The longest side of a polygon is smaller than the sum of its other sides.
+
+Conversely, if you have k (k >= 3) positive real numbers a1, a2, a3, ..., ak where a1 <= a2 <= a3 <= ... <= ak and a1 + a2 + a3 + ... + ak-1 > ak, then there always exists a polygon with k sides whose lengths are a1, a2, a3, ..., ak.
+
+The perimeter of a polygon is the sum of lengths of its sides.
+
+Return the largest possible perimeter of a polygon whose sides can be formed from nums, or -1 if it is not possible to create a polygon.
+
+Example 1:
+
+Input: nums = [5,5,5]
+Output: 15
+Explanation: The only possible polygon that can be made from nums has 3 sides: 5, 5, and 5. The perimeter is 5 + 5 + 5 = 15.
+
+Example 2:
+
+Input: nums = [1,12,1,2,5,50,3]
+Output: 12
+Explanation: The polygon with the largest perimeter which can be made from nums has 5 sides: 1, 1, 2, 3, and 5. The perimeter is 1 + 1 + 2 + 3 + 5 = 12.
+We cannot have a polygon with either 12 or 50 as the longest side because it is not possible to include 2 or more smaller sides that have a greater sum than either of them.
+It can be shown that the largest possible perimeter is 12.
+
+Example 3:
+
+Input: nums = [5,5,50]
+Output: -1
+Explanation: There is no possible way to form a polygon from nums, as a polygon has at least 3 sides and 50 > 5 + 5.
+
+Constraints:
+3 <= n <= 105
+1 <= nums[i] <= 109
+*/
+
+//Approach 1 
+
+class Solution {
+public:
+    long long largestPerimeter(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<long long int> currSum(n,0);
+        long long int sum=0;
+        for(int i=1;i<n;i++){
+            sum+=nums[i-1];
+            currSum[i]=sum;
+        }
+
+        for(int j=n-1;j>=0;j--){
+            if(currSum[j]>nums[j]){
+                return currSum[j]+nums[j];
+            }
+        }
+        return -1;
+        
+    }
+};
+
+//Approach 2
+
+class Solution {
+public:
+    long long largestPerimeter(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        long long int sum=nums[0]+nums[1];
+        long long int output=-1;
+        for(int i=2;i<n;i++){
+            if(sum>nums[i]){
+                output=max(output,sum+nums[i]);
+            }
+            sum+=nums[i];
+        }
+        return output;
+        
+    }
+};
