b41d49f95e79a114da4bff20ca2c35554d161c93
anshuKumar99 <148548385+anshuKumar99@users.noreply.github.com>
2024-03-22 15:57:24 +0530

    Create Leetcode_206_Reverse Linked List.cpp

---
+++ Leetcode_206_Reverse Linked List.cpp
@@ -0,0 +1,63 @@
+/*
+
+LEETCODE LINK : https://leetcode.com/problems/reverse-linked-list/description/
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        stack<ListNode*> st;
+        while(head!=NULL){
+            st.push(head);
+            head=head->next;
+        }
+
+        ListNode* dummy=new ListNode(0);
+        ListNode* temp=dummy;
+        while(!st.empty()){
+            dummy->next=st.top();
+            dummy=dummy->next;
+            st.pop();
+        }
+        dummy->next=NULL;
+        return temp->next;
+        
+    }
+};
