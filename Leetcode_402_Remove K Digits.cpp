20baee44c59077a1086f2f6a3255703a3174a08c
anshuKumar99 <148548385+anshuKumar99@users.noreply.github.com>
2024-04-14 02:52:16 +0530

    Update Leetcode_402_Remove K Digits.cpp

--- Leetcode_402_Remove K Digits.cpp
+++ Leetcode_402_Remove K Digits.cpp
@@ -32,6 +32,41 @@ num does not have any leading zeros except for the zero itself.
 
 */
 
+// Approach Using String
+
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        int n=num.size();
+        if(k==n){
+            return "0";
+        }
+        string ans="";
+
+        for(int i=0;i<n;i++){
+            while(ans.size()>0 && ans.back()>num[i] && k>0){
+                ans.pop_back();
+                k--;
+            }
+            if(ans.size()>0 || num[i]!='0') ans.push_back(num[i]);
+        }
+
+        while(ans.size()>0 && k>0){
+            ans.pop_back();
+            k--;
+        }
+       
+        if(ans==""){
+            return "0";
+        }
+
+        return ans;
+
+    }
+
+};
+
+
 // Appraoch Using Stack
 
 class Solution {
