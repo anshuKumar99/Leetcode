16c98f191780656901fa876d2ad8443c395ba94d
anshuKumar99 <148548385+anshuKumar99@users.noreply.github.com>
2024-05-22 14:27:00 +0530

    Update Leetcode_1863_Sum of All Subset XOR Totals.cpp

--- Leetcode_1863_Sum of All Subset XOR Totals.cpp
+++ Leetcode_1863_Sum of All Subset XOR Totals.cpp
@@ -111,3 +111,19 @@ public:
         return output;
     }
 };
+
+// APPROACH 3: OBSERVING PATTERN
+// T.C : O(n)
+// S.C : O(1)
+
+class Solution {
+public:
+     int subsetXORSum(vector<int>& nums) {
+        int n=nums.size();
+        int output=0;
+        for(int i=0;i<n;i++){
+            output=output | nums[i];
+        }
+        return output << (n-1);
+    }
+};
