9c276907f5ef31812c3648daedfbb313c184046e
anshuKumar99 <148548385+anshuKumar99@users.noreply.github.com>
2024-08-07 16:18:08 +0530

    Update Leetcode_3016_Minimum Number of Pushes to Type Word II.cpp

--- Leetcode_3016_Minimum Number of Pushes to Type Word II.cpp
+++ Leetcode_3016_Minimum Number of Pushes to Type Word II.cpp
@@ -63,3 +63,23 @@ Constraints:
 word consists of lowercase English letters.
 
 */
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26,0);
+        for(char &ch:word){
+            freq[ch-'a']++;
+        }
+        sort(freq.begin(),freq.end(),greater<int>());
+
+        int result=0;
+        for(int i=0;i<26;i++){
+            int press=(i/8)+1;
+            result+=press*freq[i];
+        }
+
+        return result;
+
+    }
+};
